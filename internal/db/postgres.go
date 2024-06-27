924ae023ef406629945765a4ea925ef5f698b1c2
Nikita Khvorostin <khvorostinn@gmail.com>
2024-06-27 03:58:55 +0300

    Refractored

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,15 +1,18 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
 
 type PostgresDB struct {
 	conn *sql.DB
 }
 
-func NewPostgredDB(connStr string) (*PostgresDB, error) {
+func NewPostgresDB(connStr string) (*PostgresDB, error) {
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return nil, err
 	}
-	return &PostgresDB{conn: db}, err
+	return &PostgresDB{conn: db}, nil
 }
