b2a0983d8bdc71ab69ecd7e98cf9c0ac5881693a
Nikita Khvorostin <khvorostinn@gmail.com>
2024-06-27 05:30:11 +0300

    Refactored comments

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -3,7 +3,7 @@ package models
 type Comment struct {
 	ID       string    `json:"id"`
 	PostID   string    `json:"postId"`
-	ParentID *string   `json:"parentID"`
+	ParentID *string   `json:"parentId"`
 	Content  string    `json:"content"`
 	Children []Comment `json:"children"`
 }
